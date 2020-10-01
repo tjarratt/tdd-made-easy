e4d69dd923b0fe047c1f632a112d26f443899f78
Tim Jarratt <tjarratt@pivotal.io>
2020-10-01 15:19:37 +0200

    Fourth test case
    Okay now we clearly want to handle adding as many integers as possible.
    It's okay to assume here that there will be many integers, enough to
    iterate over, and certainly enough to fit within memory.

--- string-calculator/test/calculator_test.rb
+++ string-calculator/test/calculator_test.rb
@@ -14,4 +14,8 @@ class CalculatorTest < Minitest::Test
   def test_sum_of_two_numbers
     assert_equal 3, Calculator::add("1,2")
   end
+
+  def test_sum_of_many_comma_separated_numbers
+    assert_equal 55, Calculator::add("1,2,3,4,5,6,7,8,9,10")
+  end
 end
