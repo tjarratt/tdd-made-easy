4daa63ade441db1eaea355803a54eed29a8a888d
Tim Jarratt <tjarratt@pivotal.io>
2020-10-01 15:16:49 +0200

    Second test case
    Handle the simplest non-trivial input, a single integer.
    
    Don't over think it, what's the simplest code that makes this work ?

--- string-calculator/test/calculator_test.rb
+++ string-calculator/test/calculator_test.rb
@@ -6,4 +6,8 @@ class CalculatorTest < Minitest::Test
   def test_empty_string_equals_zero
     assert_equal 0, Calculator::add("")
   end
+
+  def test_single_number_equals_itself
+    assert_equal 1, Calculator::add("1")
+  end
 end
