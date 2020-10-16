4c55073a90d62391814a47781eaefb5e26fe9a8e
Tim Jarratt <tjarratt@gmail.com>
2020-10-16 12:32:38 +0200

    Fourth solution
    Finally starting to get somewhere.

--- string-calculator/lib/calculator.rb
+++ string-calculator/lib/calculator.rb
@@ -2,7 +2,6 @@ module Calculator
   def self.add(args)
     return 0 if args.empty?
 
-    return 1 if args.size == 1
-    return 3 if args.size == 3
+   args.split(",").map(&:to_i).inject(:+)
   end
 end
