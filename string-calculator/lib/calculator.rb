2cdd3de7783b722a5bcb2a13590593b4b47a641d
Tim Jarratt <tjarratt@gmail.com>
2020-10-16 12:37:35 +0200

    Sixth solution

--- string-calculator/lib/calculator.rb
+++ string-calculator/lib/calculator.rb
@@ -2,6 +2,10 @@ module Calculator
   def self.add(args)
     return 0 if args.empty?
 
+    if args.start_with?("//")
+      return args.split(args[2]).map(&:to_i).inject(:+)
+    end
+
     args.split(/[,\n]/).map(&:to_i).inject(:+)
   end
 end
