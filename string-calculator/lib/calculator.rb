275e4ff1ae9affb1364e1c328c3c376e1e2beeda
Tim Jarratt <tjarratt@gmail.com>
2020-10-16 15:35:29 +0200

    Ninth solution

--- string-calculator/lib/calculator.rb
+++ string-calculator/lib/calculator.rb
@@ -7,7 +7,7 @@ module Calculator
       .split(iterator)
       .map(&:to_i)
       .tap {|input| validate_non_negative(input) }
-      .filter {|input| input != 1000 }
+      .filter {|input| input < 1000 }
       .inject(:+)
   end
 
